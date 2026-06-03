9af22d79736d91f452cdbca4eb61518dd7250f22
rcy1314 <rcy13141314@gmail.com>
2026-06-03 17:13:01 +0800

    修复并增强 Service Worker 的静态资源缓存

--- themes/noisedh-nav-v2/static/service-worker.js
+++ themes/noisedh-nav-v2/static/service-worker.js
@@ -1,24 +1,61 @@
-var cacheName = 'Noise导航-2.7-20260520';
+var cacheName = 'Noise导航-2.7-20260603';
+
+function withScope(path) {
+  return new URL(path, self.registration.scope).toString();
+}
+
+function normalizeRequest(request) {
+  try {
+    var url = new URL(request.url);
+    url.search = '';
+    url.hash = '';
+    return new Request(url.toString(), {
+      method: 'GET',
+      mode: request.mode,
+      credentials: request.credentials,
+      redirect: request.redirect
+    });
+  } catch (e) {
+    return request;
+  }
+}
+
+var precacheList = [
+  './',
+  './index.html',
+  './manifest.json',
+  './assets/css/iconfont-3.03029.1.css',
+  './assets/css/bootstrap.min-4.3.1.css',
+  './assets/css/styles.css',
+  './assets/css/loading.css',
+  './assets/css/custom-style.css',
+  './assets/css/sticker-theme.css',
+  './assets/css/APlayer.min.css',
+  './assets/css/hot.css',
+  './assets/fontawesome-5.15.4/css/all.min.css',
+  './assets/fontawesome-5.15.4/css/v4-shims.min.css',
+  './assets/js/jquery.min.js',
+  './assets/js/jquery.ui.touch-punch.min-0.2.2.js',
+  './assets/js/tooltip-extend.js',
+  './assets/js/bootstrap.min-4.3.1.js',
+  './assets/js/theia-sticky-sidebar-1.5.0.js',
+  './assets/js/APlayer.min.js',
+  './assets/js/Meting.min.js',
+  './assets/js/app-mini.js',
+  './assets/js/hot.js',
+  './assets/images/favicon.png',
+  './assets/images/favicon4.png'
+];
 
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(cacheName).then(function(cache) {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/manifest.json',
-        '/assets/css/styles.css',
-        '/assets/css/custom-style.css',
-        '/assets/css/loading.css',
-        '/assets/css/sticker-theme.css',
-        '/assets/fontawesome-5.15.4/css/all.min.css',
-        '/assets/js/app-mini.js',
-        '/assets/js/tooltip-extend.js',
-        '/assets/js/bootstrap.min-4.3.1.js',
-        '/assets/js/hot.js',
-        '/assets/images/favicon.png',
-        '/assets/images/favicon4.png'
-      ]);
+      return Promise.all(
+        precacheList.map(function(path) {
+          return cache.add(withScope(path)).catch(function() {
+          });
+        })
+      );
     }).then(function() {
       return self.skipWaiting();
     })
@@ -72,7 +109,15 @@ function isHtmlRequest(request) {
 
 function isStaticAssetRequest(request) {
   var destination = request.destination || '';
-  return destination === 'script' || destination === 'style' || destination === 'worker' || destination === 'image' || destination === 'font';
+  if (destination === 'script' || destination === 'style' || destination === 'worker' || destination === 'image' || destination === 'font') {
+    return true;
+  }
+  try {
+    var pathname = new URL(request.url).pathname || '';
+    return /\.(css|js|mjs|png|jpg|jpeg|gif|webp|svg|ico|woff|woff2|ttf|otf|eot)$/.test(pathname);
+  } catch (e) {
+    return false;
+  }
 }
 
 // 网络优先：优先获取最新内容，失败时使用缓存
@@ -91,17 +136,28 @@ function networkFirst(request) {
 
 // 缓存优先：立即返回缓存，不等待网络
 function cacheFirst(request) {
-  return caches.match(request).then(function(cachedResponse) {
-    if (cachedResponse) return cachedResponse;
+  var normalizedRequest = normalizeRequest(request);
+  return caches.match(normalizedRequest).then(function(cachedResponse) {
+    if (cachedResponse) {
+      fetch(request).then(function(networkResponse) {
+        if (networkResponse && networkResponse.ok) {
+          caches.open(cacheName).then(function(cache) {
+            cache.put(normalizedRequest, networkResponse.clone());
+          });
+        }
+      }).catch(function() {
+      });
+      return cachedResponse;
+    }
     return fetch(request).then(function(networkResponse) {
       if (networkResponse && networkResponse.ok) {
         caches.open(cacheName).then(function(cache) {
-          cache.put(request, networkResponse.clone());
+          cache.put(normalizedRequest, networkResponse.clone());
         });
       }
       return networkResponse;
     }).catch(function() {
-      return cachedResponse;
+      return caches.match(normalizedRequest);
     });
   });
 }
